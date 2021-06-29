4d52eba21085d66ef340fe4730524950c38442d4
muhammad-anas-2 <bitf16a537@pucit.edu.pk>
2021-06-28 21:13:36 -0700

    Add error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated." }
+        format.html { redirect_to @article, notice:  "Article was successfully updated." }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
